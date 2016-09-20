5ffce9eabd8f56c434484bd46c0b164be72e291e
mcueto <cueto@live.cl>
2016-09-20 13:22:45 -0300

    Minor changes

--- alerts/models.py
+++ alerts/models.py
@@ -7,10 +7,34 @@ class App(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
+class EntityType(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30, blank=True)
+    app = models.ForeignKey(App)
+
+    def __str__(self):
+        return self.name
+
+class Entity(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30, blank=True)
+    entity_type = models.ForeignKey(EntityType)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 class AlertType(models.Model):
     name = models.CharField(max_length=10)
-    description = models.CharField(max_length=30)
     app = models.ForeignKey(App)
+    description = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Alert(models.Model):
     name = models.CharField(max_length=30)
@@ -18,15 +42,7 @@ class Alert(models.Model):
     alert_type = models.ForeignKey(AlertType)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    entity = models.ForeignKey(Entity)
 
-class EntityType(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
-    app = models.ForeignKey(App)
-
-class Entity(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
-    entity_type = models.ForeignKey(EntityType)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
