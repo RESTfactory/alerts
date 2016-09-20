5c6b11564b7c1d7ee5a447aed3e78fe5f0e98dc9
mcueto <cueto@live.cl>
2016-09-20 12:52:59 -0300

    Added relation within App and alertype and entitytype

--- alerts/models.py
+++ alerts/models.py
@@ -1,9 +1,16 @@
 from django.conf import settings
 from django.db import models
 
+class App(models.Model):
+    name = models.CharField(max_length=30)
+    # owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class AlertType(models.Model):
     name = models.CharField(max_length=10)
     description = models.CharField(max_length=30)
+    app = models.ForeignKey(App)
 
 class Alert(models.Model):
     name = models.CharField(max_length=30)
@@ -15,16 +22,11 @@ class Alert(models.Model):
 class EntityType(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=30)
+    app = models.ForeignKey(App)
 
 class Entity(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=30)
     entity_type = models.ForeignKey(EntityType)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class App(models.Model):
-    name = models.CharField(max_length=30)
-    # owner = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
