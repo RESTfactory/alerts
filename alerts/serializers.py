a84c720bc26fb1b9d4d98fea6ea67bad60e8ed98
mcueto <cueto@live.cl>
2016-10-28 13:25:28 -0300

    Removed jsonapi and added explicit field to serializers

--- alerts/serializers.py
+++ alerts/serializers.py
@@ -4,20 +4,24 @@ from .models import AlertType, Alert, EntityType, Entity, App
 class AlertTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = AlertType
-        # fields = ('url', 'username', 'email', 'is_staff')
+        fields = ('url', 'id', 'name', 'app', 'description')
 
 class AlertSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model =Alert
+        fields = ('url', 'id', 'name', 'description', 'alert_type', 'created_at', 'updated_at', 'entity')
 
 class EntityTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = EntityType
+        fields = ('url', 'id', 'name', 'description', 'app')
 
 class EntitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Entity
+        fields = ('url', 'id', 'name', 'description', 'entity_type', 'created_at', 'updated_at')
 
 class AppSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = App
+        fields = ('url', 'id', 'name', 'created_at', 'updated_at')
