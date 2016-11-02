e99e80ecdf6d9c8d73f2e062dc4976dd1ec025a6
mcueto <cueto@live.cl>
2016-11-02 12:44:27 -0300

    Changed hyperlinkedmodelserializers to modelserializers

--- alerts/serializers.py
+++ alerts/serializers.py
@@ -1,27 +1,27 @@
 from rest_framework import serializers
 from .models import AlertType, Alert, EntityType, Entity, App
 
-class AlertTypeSerializer(serializers.HyperlinkedModelSerializer):
+class AlertTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AlertType
         fields = ('url', 'id', 'name', 'app', 'description')
 
-class AlertSerializer(serializers.HyperlinkedModelSerializer):
+class AlertSerializer(serializers.ModelSerializer):
     class Meta:
         model =Alert
         fields = ('url', 'id', 'name', 'description', 'alert_type', 'created_at', 'updated_at', 'entity')
 
-class EntityTypeSerializer(serializers.HyperlinkedModelSerializer):
+class EntityTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = EntityType
         fields = ('url', 'id', 'name', 'description', 'app')
 
-class EntitySerializer(serializers.HyperlinkedModelSerializer):
+class EntitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entity
         fields = ('url', 'id', 'name', 'description', 'entity_type', 'created_at', 'updated_at')
 
-class AppSerializer(serializers.HyperlinkedModelSerializer):
+class AppSerializer(serializers.ModelSerializer):
     class Meta:
         model = App
         fields = ('url', 'id', 'name', 'created_at', 'updated_at')
